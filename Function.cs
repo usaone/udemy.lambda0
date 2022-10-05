73485f8c92ecf674780ce9f61f9aa0170cae9765
usaone <uddipmitra@gmail.com>
2022-10-05 03:23:31 -0400

    Add Post Handler and Refactor code

--- Function.cs
+++ Function.cs
@@ -2,6 +2,7 @@ using Amazon.DynamoDBv2;
 using Amazon.DynamoDBv2.DataModel;
 using Amazon.Lambda.APIGatewayEvents;
 using Amazon.Lambda.Core;
+using System.Text.Json;
 
 // Assembly attribute to enable the Lambda function's JSON input to be converted into a .NET class.
 [assembly: LambdaSerializer(typeof(Amazon.Lambda.Serialization.SystemTextJson.DefaultLambdaJsonSerializer))]
@@ -10,22 +11,85 @@ namespace udemy.lambda;
 
 public class Function
 {
-    
+    private readonly DynamoDBContext _dynamoDBContext;
+
+    public Function()
+    {
+        _dynamoDBContext = new DynamoDBContext(new AmazonDynamoDBClient());
+    }
+
+
     /// <summary>
     /// A simple function that takes a string and does a ToUpper
     /// </summary>
     /// <param name="input"></param>
     /// <param name="context"></param>
     /// <returns></returns>
-    public async Task<User> FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    public async Task<APIGatewayHttpApiV2ProxyResponse> FunctionHandler(APIGatewayHttpApiV2ProxyRequest request, ILambdaContext context)
+    {
+        return request.RequestContext.Http.Method.ToUpper() switch
+        {
+            "GET" => await HandleGetRequest(request),
+            "POST" => await HandlePostRequest(request)
+        };
+    }
+
+    private async Task<APIGatewayHttpApiV2ProxyResponse> HandlePostRequest(APIGatewayHttpApiV2ProxyRequest request)
+    {
+        var user = JsonSerializer.Deserialize<User>(request.Body);
+        if (user == null)
+        {
+            return BadResponse400("Invalid input.");
+        }
+
+        await _dynamoDBContext.SaveAsync(user);
+
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            Body = request.Body,
+            StatusCode = 200
+        };
+    }
+
+    private async Task<APIGatewayHttpApiV2ProxyResponse> HandleGetRequest(APIGatewayHttpApiV2ProxyRequest request)
     {
         request.PathParameters.TryGetValue("userId", out var userIdString);
-        Guid.TryParse(userIdString, out var userId);
+        if (Guid.TryParse(userIdString, out var userId))
+        {
+            var user = await _dynamoDBContext.LoadAsync<User>(userId);
 
-        var dynamoDBContext = new DynamoDBContext(new AmazonDynamoDBClient());
-        var user = await dynamoDBContext.LoadAsync<User>(userId);
+            if (user != null)
+            {
 
-        return user;
+                return new APIGatewayHttpApiV2ProxyResponse
+                {
+                    Body = JsonSerializer.Serialize(user),
+                    StatusCode = 200
+                };
+            }
+            else
+                return NotFound404("UserId does not exist.");
+        }
+
+        return BadResponse400("Invalid UserId.");
+    }
+
+    private static APIGatewayHttpApiV2ProxyResponse BadResponse400(string message)
+    {
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            Body = "Bad Response. " + message,
+            StatusCode = 400
+        };
+    }
+
+    private static APIGatewayHttpApiV2ProxyResponse NotFound404(string message)
+    {
+        return new APIGatewayHttpApiV2ProxyResponse
+        {
+            Body = "Not Found. " + message,
+            StatusCode = 404
+        };
     }
 
     /// <summary>
